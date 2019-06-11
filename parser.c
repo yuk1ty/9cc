41d125d67b0a8907153f6520c27b670f83b618e9
yuk1ty <yuki.mul.tiplus@gmail.com>
2019-06-12 01:40:23 +0900

    return 文をトークナイズできるようにした

--- parser.c
+++ parser.c
@@ -15,6 +15,13 @@ Token *new_token_val(int ty, int val, char *input) {
     return token;
 }
 
+int is_alnum(char c) {
+    return ('a' <= c && c <= 'z') ||
+            ('A' <= c && c <= 'Z') ||
+            ('0' <= c && c <= '9') ||
+            (c == '_');
+}
+
 void tokenize() {
     tokens = new_vector();
     char *p = user_input;
@@ -66,6 +73,14 @@ void tokenize() {
             vec_push(tokens, (void *) token);
             i++;
             p++;
+            continue;
+        }
+
+        if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
+            Token *token = new_token(TK_RETURN, p);
+            vec_push(tokens, (void *) token);
+            i++;
+            p += 6;
             continue;
         }
 
